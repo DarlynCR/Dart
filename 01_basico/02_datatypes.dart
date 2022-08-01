8b70f90270650cc7cb3cb25cb8b3caec52bc20f0
DarlynCR <darlyn.caicedo.calderon@gmail.com>
2022-08-01 16:13:13 -0500

    initial commit

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,30 @@
+
+
+main(){
+
+    //Números
+    int a = 10;
+    a = 20;
+    a = 10;
+
+    //int x,y,z;
+    int r = 10, y=20, z=30;
+    print(a);
+
+
+
+    //Strings
+
+    //Booleans
+
+
+    //Arrays
+
+
+
+    //Sets
+
+
+
+    //Maps
+}

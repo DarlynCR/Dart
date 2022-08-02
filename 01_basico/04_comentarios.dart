d68859bcc7f0e72429f1ca4d32c8cc0a38b10d77
DarlynCR <darlyn.caicedo.calderon@gmail.com>
2022-08-02 16:10:37 -0500

    comentarios en dart

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,33 @@
+/**
+* Comentarios para documentación multilinea
+*[CreadoPor] : Darlyn Caicedo
+*
+*/
+
+/// Comentarios de documentación de 1 línea
+main(){
+
+    //Este es un comentario
+    int a = 0; //Aquí también se puede poner un comentario
+
+    final persons = [
+        //Aquí también se puede comentar
+        'Sergio', //Aquí también se puede comentar
+        'Darlyn',
+        'Bebé Barrigas'
+        //Y aquí
+    ]
+
+
+    /* Este es un comentario
+    multilinea
+    multilinea
+     */
+
+
+}
+
+//Este es otro comentario
+//Este es un comentario de documentación en dartdoc
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(String nombre, String mensaje){}

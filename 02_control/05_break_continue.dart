b8d2d1eda5f5e82fc548abebc73944e413dfdbe6
DarlynCR <darlyn.caicedo.calderon@gmail.com>
2022-08-04 14:48:08 -0500

    break - continue

---
+++ 02_control/05_break_continue.dart
@@ -0,0 +1,27 @@
+
+
+
+main() {
+
+
+    for (int i = 0; i < 10; i++){
+
+      if(i == 5 ){
+        continue; //salta a la sig iteración
+      }
+      
+      if(i == 8 ){
+        break; //sale del ciclo
+      }
+
+      print(i);
+
+
+
+
+    }
+
+
+
+
+}

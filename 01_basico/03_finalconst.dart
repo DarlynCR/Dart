7c87bddc760128396661c27553a3f31410a00b1c
DarlynCR <darlyn.caicedo.calderon@gmail.com>
2022-08-09 15:41:50 -0500

    Se agrega declaración con late

--- 01_basico/03_finalconst.dart
+++ 01_basico/03_finalconst.dart
@@ -3,15 +3,16 @@ main(){
 
     var a   = 10; 
     // Definición de variables que no cambiarán después de su definición
-    final b = 20; // Es más ligera en memoria
-    const c = 30; //Crea el espacio de memoria en tiempo de compilación
+    final b = 20; // Una vez inicializada no se puede modificar su valor
+    const c = 30; //Crea el espacio de memoria en tiempo de compilación - sin métodos de lectura, ni para establecer sus valores
+    late final e; // Una vez inicializada no se puede modificar su valor
 
     final double d =40;
     //Con var no se puede
 
     final personFinal = ['Sergio', 'Darlyn']; //Sólo se pueden agregar mediante sus métodos
     const personnConst =['Sergio', 'Darlyn']; //No se pueden agregar más elementos
-    List<String> personnConst2 = const['Sergio', 'Darlyn']; 
+    List<String> personnConst2 = const['Sergio', 'Darlyn']; // otra forma de declarar
 
 
     personFinal.add('Bebé Barrigas');
